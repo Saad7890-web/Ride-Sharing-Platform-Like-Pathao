a4ea5e49b5fd57e82a602200822422ba624294b0
Saad7890-web <omysaadislam@gmail.com>
2026-01-11 22:12:05 +0600

    remove the logs

--- internal/auth/middleware/permission_middleware.go
+++ internal/auth/middleware/permission_middleware.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 
 	"ride/internal/auth/repository"
@@ -15,7 +14,7 @@ func PermissionMiddleware(
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			userID := GetUserID(r.Context())
-			log.Println("USER ID FROM TOKEN:", userID)
+			
 			if userID == "" {
 				http.Error(w, "unauthorized", http.StatusUnauthorized)
 				return
